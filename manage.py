25eae3528a4d10ed84d7427f671052b1f1e7af52
Sebastian Morales <sebastian-morales@sebasmoralesd.com>
2024-06-11 17:22:08 -0500

    Host y Puerto establecidos a 0.0.0.0:8000 por defecto

--- manage.py
+++ manage.py
@@ -7,6 +7,10 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_core.settings')
+    default_addr = '0.0.0.0'
+    default_port = '8000'
+    default_url = f"{default_addr}:{default_port}"
+    
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,7 +19,13 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+        
+    if len(sys.argv) == 2 and "runserver" in sys.argv:
+        execute_from_command_line(
+            sys.argv + [default_url]
+        )
+    else:
+        execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
